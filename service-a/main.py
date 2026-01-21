4810cccbdba6fe69bd292b4a3272bbfbd1a678d6
Yehuda Freiman <yehfre@icloud.com>
2026-01-21 09:09:26 +0200

    update main of service a

--- service-a/main.py
+++ service-a/main.py
@@ -1,12 +1,17 @@
 from fastapi import FastAPI
 from api_extractor import ingest_weather_for_location
 from fastapi.encoders import jsonable_encoder
+import requests
 
 app = FastAPI()
 
 @app.post("/ingest")
 def read_external_data():
-    # this triggers the whole pipeline to ingest weather for London and write to mysql
     data = ingest_weather_for_location("London")
     json_data = jsonable_encoder(data)
-    return json_data
+
+    url = "http://0.0.0.0:8001"
+    response = requests.post(url, json_data)
+
+    return response
+
