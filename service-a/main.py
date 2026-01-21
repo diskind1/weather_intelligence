3145799ada5e234d59f2e5f71f401acf40d220d1
diskind1 <menni.diskind@gmail.com>
2026-01-21 09:30:25 +0200

    update mains of service a and service b

--- service-a/main.py
+++ service-a/main.py
@@ -3,15 +3,17 @@ from api_extractor import ingest_weather_for_location
 from fastapi.encoders import jsonable_encoder
 import requests
 
-app = FastAPI()
+app = FastAPI(title="Service A - Ingestion")
 
 @app.post("/ingest")
 def read_external_data():
     data = ingest_weather_for_location("London")
     json_data = jsonable_encoder(data)
 
-    url = "http://0.0.0.0:8001"
-    response = requests.post(url, json_data)
+    url = "http://127.0.0.1:8002/clean"
+    response = requests.post(url, json=json_data)
 
-    return response
+
+
+    return response.json
 
--- service-b/main.py
+++ service-b/main.py
@@ -1,18 +1,32 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Body
 import uvicorn
 
 app = FastAPI(title="Service B - Data Cleaning & Normalization")
 
 @app.post("/clean")
 
-def clean(data):
+def clean(data = Body(...)):
+    return {"received_type": str(type(data)), "received": data}
 
-    clean_data = [data]
-    # for loc in data:
-    #     clean_data.append(loc.get("location_name"))
+    # clean_data = [data]
+    # # for loc in data:
+    # #     clean_data.append(loc.get("location_name"))
 
-    return clean_data
+    # return clean_data
 
 
 if __name__ == "__main__":
     uvicorn.run(app, host="0.0.0.0", port=8002)
+
+
+
+
+
+
+
+
+    # data יהיה כל ה-JSON שנשלח בגוף הבקשה (Body)
+
+if __name__ == "__main__":
+    uvicorn.run(app, host="0.0.0.0", port=8002)
+
